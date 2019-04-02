3290c7d2444dfb11874149890b722b5d9fb680bf
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-02 17:07:05 -0400

    Lewis Template 1.0  /posts.vue

--- posts.vue
+++ posts.vue
@@ -3,11 +3,11 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-                    <div class="main_container">
-                        <h2>Promotions</h2>
-                    </div>
-                </div>
+                <!--<div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">-->
+                <!--    <div class="main_container">-->
+                <!--        <h2>Promotions</h2>-->
+                <!--    </div>-->
+                <!--</div>-->
                 <div v-if="postList">
                     <div class="row"  v-for="(item, index) in postList">
                         <div class="col-xs-5 blogpost_img">
