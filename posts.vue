a59d6d332b06a1fa074a4144b112751dad2ea0ed
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-02 16:45:03 -0400

    Lewis Template 1.0  /posts.vue

--- posts.vue
+++ posts.vue
@@ -48,29 +48,33 @@
                     'findBlogByName'
                 ]),
                 postList() {
-                    var blog = this.findBlogByName("Main Blog").posts;
-                    var vm = this;
-                    var temp_blog = [];
-                    _.forEach(blog, function(value, key) {
-                        var today = moment().tz(vm.timezone);
-                        var webDate = moment(value.publish_date).tz(vm.timezone);
-                        if (today >= webDate) {
-                            if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/jpeg/1527006352000/bccblogplaceholder.jpg";
+                    var blog = this.findBlogByName("Main Blog");
+                    if (blog){
+                        blog = blog.posts
+        
+                        var vm = this;
+                        var temp_blog = [];
+                        _.forEach(blog, function(value, key) {
+                            var today = moment().tz(vm.timezone);
+                            var webDate = moment(value.publish_date).tz(vm.timezone);
+                            if (today >= webDate) {
+                                if (_.includes(value.image_url, 'missing')) {
+                                    value.image_url = "//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/jpeg/1527006352000/bccblogplaceholder.jpg";
+                                }
+                                
+                                if (value.body.length > 99) {
+                                    value.body_short = _.truncate(value.body, { 'length': 99, 'separator': ' ' });
+                                } else {
+                                    value.body_short = value.body;
+                                }
+                                
+                                temp_blog.push(value);
                             }
-                            
-                            if (value.body.length > 99) {
-                                value.body_short = _.truncate(value.body, { 'length': 99, 'separator': ' ' });
-                            } else {
-                                value.body_short = value.body;
-                            }
-                            
-                            temp_blog.push(value);
-                        }
-                    });
-                    blog = _.reverse(_.sortBy(temp_blog, function (o) { return o.publish_date }));
-                    return blog
-                }
+                        });
+                        blog = _.reverse(_.sortBy(temp_blog, function (o) { return o.publish_date }));
+                        return blog
+                    }
+                }   
             },
             methods: {
                 loadData: async function () {
