7b89708937655e33845a29d3bea1ecde88dfb5c3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-02 17:07:55 -0400

    Lewis Template 1.0  /posts.vue

--- posts.vue
+++ posts.vue
@@ -3,11 +3,11 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <!--<div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">-->
-                <!--    <div class="main_container">-->
-                <!--        <h2>Promotions</h2>-->
-                <!--    </div>-->
-                <!--</div>-->
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+                    <div class="main_container">
+                        <h2>Promotions</h2>
+                    </div>
+                </div>
                 <div v-if="postList">
                     <div class="row"  v-for="(item, index) in postList">
                         <div class="col-xs-5 blogpost_img">
@@ -55,9 +55,9 @@
                     // if(temp_repo != null) {
                     //     this.pageBanner = temp_repo[0];
                     // } else {
-                    //     this.pageBanner = {
-                    //         "image_url": "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531495616000/inside_banner.png"
-                    //     }
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531495616000/inside_banner.png"
+                        }
                     // }
                     
                     this.dataLoaded = true;
