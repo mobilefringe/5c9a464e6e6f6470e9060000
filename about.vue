4b81e56debd59f9f837a7c40f8973a9e23e080ac
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-05 15:02:14 -0400

    Lewis Template 1.0  /about.vue

--- about.vue
+++ about.vue
@@ -62,7 +62,7 @@
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
                         let results = await Promise.all([
-                            this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/"+ this.property.slug + "about-us.json" })
+                            this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/"+ this.property.slug + "-about-us.json" })
                         ]);
                         return results;
                     } catch (e) {
