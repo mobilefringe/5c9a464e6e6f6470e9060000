17135f305842eb1487633360c8d7674f922a066b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-05 14:53:39 -0400

    Lewis Template 1.0  /directions.vue

--- directions.vue
+++ directions.vue
@@ -15,7 +15,7 @@
                         <div class="col-md-12">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3302.5050034806413!2d-117.61748678478888!3d34.13341998058389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c33743971a8557%3A0x2c1fbba344c1504d!2sAlta+Loma+Square!5e0!3m2!1sen!2sca!4v1554413797361!5m2!1sen!2sca" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
                             <hr>
-                            <div class="contact_page_body" v-if="currentPage.body" v-html="currentPage.body"></div>
+                            <div class="contact_page_body" v-if="currentPage" v-html="currentPage.body"></div>
                         </div>
                     </div>
                 </div>
