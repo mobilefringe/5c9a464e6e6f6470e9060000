956b00e020dc7a62ad4c47f47c916afc98999425
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-05 14:22:51 -0400

    Lewis Template 1.0  /sales_and_events.vue

--- sales_and_events.vue
+++ sales_and_events.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_header_background">
                     <div class="main_container">
-                        <h2>Events & Promotions</h2>
+                        <h2>Sales & Events</h2>
                     </div>
                 </div>
                 <div class="main_container mobile_padding margin_30">
@@ -129,6 +129,15 @@
             },
             created (){
                 this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Inside Page Banner').images;
+                    if (temp_repo != null) {
+                        this.pageBanner = temp_repo[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531495616000/inside_banner.png"
+                        }
+                    }
+                    
                     this.dataLoaded = true;
                 });
             },
