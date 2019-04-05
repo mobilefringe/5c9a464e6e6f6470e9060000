9f1494468dfd1355461d7572e29af4ab211d12a4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-05 14:24:54 -0400

    Lewis Template 1.0  /sales_and_events.vue

--- sales_and_events.vue
+++ sales_and_events.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background">
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container">
                         <h2>Sales & Events</h2>
                     </div>
