4676b25860dcaaa0cce9b66ed809f2db993352da
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-05 16:00:17 -0400

    Lewis Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -11,7 +11,7 @@
                 <div class="main_container mobile_padding margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/jpeg/1531500156000/sidebanner4.jpg" alt="" />    
+                            <img class="img_max" v-if="currentImage" :src="currentImage" alt="" />    
                         </div>
                         <div class="details_col_9">
                             <div class="page_body" v-html="currentPage.body"></div>
