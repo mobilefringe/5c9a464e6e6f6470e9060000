e8d1d6d39b785fef2a612c05d1be95cd560592ee
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-05 15:25:40 -0400

    Lewis Template 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -32,8 +32,10 @@
                     <p class="footer_text text-center">Powered by <a href="https://www.mallmaverick.com/" target="_blank">Mall Maverick</a></p>
                 </div>
                 <div class="col-md-4">
-                    <p class="footer_text">&#169; {{copyright_year}} {{property.name}} | <a :href="siteInfo.propertyManagementURL" target="_blank">{{ siteInfo.propertyManagementName }}</a></p>
-                    <img src="//codecloud.cdn.speedyrails.net/sites/5c9a464e6e6f6470e9060000/image/png/1554491926156/LewisRetailCentersWhite11.png" class="" alt="Lewis Retail Centers Logo">
+                    <p class="footer_text">&#169; {{copyright_year}} {{property.name}}</p>
+                    <a :href="siteInfo.propertyManagementURL" target="_blank">
+                        <img src="//codecloud.cdn.speedyrails.net/sites/5c9a464e6e6f6470e9060000/image/png/1554491926156/LewisRetailCentersWhite11.png" class="" alt="Lewis Retail Centers Logo">
+                    </a>
                 </div>
             </div>
         </section>
