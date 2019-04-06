bc0345e8ef77dc7ed63df1f0016680bb3cb8a1f8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-06 18:41:18 -0400

    Lewis Template 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <banner-component page_name="pageName"></banner-component>
+                <banner-component :page_name="pageName"></banner-component>
                 <div class="main_container margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
