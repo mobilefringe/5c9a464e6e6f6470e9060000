07cfd31a5fcd3485e510f8a44cf9a6961fd242ce
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-06 17:53:20 -0400

    Lewis Template 1.0  /about.vue

--- about.vue
+++ about.vue
@@ -52,7 +52,7 @@
                     if (this.currentPage.image_url === null) {
                         var img_repo = this.findRepoByName('Inside Page Default Side Image').images;
                         if (img_repo != null) {
-                            var image_url = img_repo[0];
+                            var image_url = img_repo[0].image_url;
                             console.log("image_url", image_url)
                             this.currentImage = image_url;
                         }
