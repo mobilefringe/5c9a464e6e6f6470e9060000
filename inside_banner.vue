9e79561430a3b1ae8730c0bec66d9768ae33b510
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-06 18:13:04 -0400

    Lewis Template 1.0  /inside_banner.vue

---
+++ inside_banner.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="inside_header_background" v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        <div class="main_container">
+            <div class="page_container">
+                <h2>About Us</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function(Vue, Vuex){
+        return Vue.component("messages-component", {
+            template: template, // the variable template will be injected
+            props:['page_name'],
+            data: function() {
+                return {
+                    pageBanner: null,
+                }
+            },
+            mounted() {
+                var temp_repo = this.findRepoByName('Inside Page Banner').images;
+                if (temp_repo != null) {
+                    this.pageBanner = temp_repo[0];
+                } else {
+                    this.pageBanner = {
+                        "image_url": "//codecloud.cdn.speedyrails.net/sites/5c9a464e6e6f6470e9060000/image/png/1554588744991/default_inside_banner.png"
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'findRepoByName'
+                ])
+            }
+        })
+    });
+</script>
