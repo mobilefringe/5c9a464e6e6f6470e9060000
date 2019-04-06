d815bfd89561dffcfc17f550f125a44421126e6f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-06 18:22:56 -0400

    Lewis Template 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -7,7 +7,7 @@
                 <div class="main_container margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="http://placehold.it/440x1200" alt="" />    
+                            <img class="img_max" v-if="currentImage" :src="currentImage" alt="" />    
                         </div>
                         <div class="details_col_9">
                             <div class="row">
