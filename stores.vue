fdc58f317ab53c2d167af06020a118bb08ccbc38
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-07 11:15:21 -0400

    Lewis Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -14,10 +14,10 @@
                     <div class="">
                         <div class="row store_nav">
                             
-                            <div class="col-md-3">
-                                <span>Sort By: </span>
-                                <a class="store_nav_link hvr-underline-from-center" v-on:click="changeMode('alphabetical')">Alphabetical</a>
-                            </div>
+                            <!--<div class="col-md-3">-->
+                            <!--    <span>Sort By: </span>-->
+                            <!--    <a class="store_nav_link hvr-underline-from-center" v-on:click="changeMode('alphabetical')">Alphabetical</a>-->
+                            <!--</div>-->
                             <div class="col-md-3">
                                 <v-select v-model="selectedCat" :options="dropDownCats" :searchable="false" :on-change="filteredByCategory" class="category-select" placeholder="Select a Category" id="selectByCat" transition="menu-fade"></v-select>
                             </div>
