deb4806997801b36516ca3683308a193a8a86bb6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-07 11:31:09 -0400

    Lewis Template 1.0  /main.js

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                 this.$store.dispatch("setDataLoaded", true); 
                 this.property.default_logo_url = "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1529701995203/default_logo_600.png";               
                 var i_banner = this.findRepoByName('Inside Page Banner');
-                if(i_banner !== null && i_banner !== undefined && i_banner.images.length > 0) {
+                if (i_banner !== null && i_banner !== undefined && i_banner.images.length > 0) {
                     this.inside_banner = i_banner.images[0];
                 }
             });
@@ -129,7 +129,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
         methods: {
             loadData: async function() {
                 try{
-                    await this.$store.dispatch('initializeApi', { site: "altalomasquare", version: "v4" });
+                    await this.$store.dispatch('initializeApi', { site: "district", version: "v4" });
                     await this.$store.dispatch("getData","property");
                     let results = await Promise.all([
                         this.$store.dispatch("INITIALIZE_LOCALE"), 
