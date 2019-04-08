7192da54e852ede3e73b7d4c7f77fc10ae4ca75b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-08 14:24:50 -0400

    Lewis Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -3,11 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-                    <div class="main_container">
-                        <h2>Promotions</h2>
-                    </div>
-                </div>
+                <banner-component :page_name="pageName"></banner-component>
                 <div class="main_container margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
