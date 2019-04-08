8fce179d86192288088cd2e743e6ee8c2908f69d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-08 13:23:08 -0400

    Lewis Template 1.0  /inside_banner.vue

--- inside_banner.vue
+++ inside_banner.vue
@@ -21,14 +21,14 @@
             },
             mounted() {
                 this.$nextTick(function() {
-                    var temp_repo = this.findRepoByName('Inside Page Banner').images;
-                    if (temp_repo != null) {
-                        this.pageBanner = temp_repo[0];
-                    } else {
+                    // var temp_repo = this.findRepoByName('Inside Page Banner').images;
+                    // if (temp_repo != null) {
+                    //     this.pageBanner = temp_repo[0];
+                    // } else {
                         this.pageBanner = {
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5c9a464e6e6f6470e9060000/image/png/1554588744991/default_inside_banner.png"
                         }
-                    }
+                    // }
                     
                     this.pageName = this.page_name;
                 });
