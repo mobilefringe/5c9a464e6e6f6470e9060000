d99c53016565aabb515c81b992be18b312e7924c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-08 17:22:57 -0400

    Lewis Template 1.0  /side_image.vue

--- side_image.vue
+++ side_image.vue
@@ -24,8 +24,6 @@
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5c9a464e6e6f6470e9060000/image/png/1554587000151/default_side_banner.png"
                         }
                     // }
-                    
-                    this.pageName = this.page_name;
                 });
             },
             computed: {
