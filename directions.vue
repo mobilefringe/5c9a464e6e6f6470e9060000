692f69ce7eabf4924faec70c1a9e860f8ce95320
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-08 17:11:32 -0400

    Lewis Template 1.0  /directions.vue

--- directions.vue
+++ directions.vue
@@ -7,7 +7,7 @@
                 <div class="main_container margin_30">
                     <div class="row">
                         <div class="col-md-12">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3302.5050034806413!2d-117.61748678478888!3d34.13341998058389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c33743971a8557%3A0x2c1fbba344c1504d!2sAlta+Loma+Square!5e0!3m2!1sen!2sca!4v1554413797361!5m2!1sen!2sca" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <iframe :src="siteInfo.googleMapURL" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
                             <div v-if="currentPage">
                                 <hr>
                                 <div class="contact_page_body" v-html="currentPage.body"></div>
@@ -20,13 +20,14 @@
     </div>
 </template>
 <script>
-    define(["Vue", "vuex", "vue!inside_banner.vue"], function(Vue, Vuex, insideBanner) {
+    define(["Vue", "vuex", "vue!inside_banner.vue", "json!site.json"], function(Vue, Vuex, insideBanner, site) {
         return Vue.component("directions-component", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
                     dataLoaded: true,
                     pageName: "Directions",
+                    siteInfo: site,
                     currentPage: null
                 }
             },
