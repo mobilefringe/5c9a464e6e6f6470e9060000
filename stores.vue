8201e626f41e669ad5179f9b1804dedc3b63546e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-08 14:58:32 -0400

    Lewis Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -7,10 +7,9 @@
                 <div class="main_container margin_30">
                     <div class="">
                         <div class="row store_nav">
-                            <div class="col-md-3">
+                            <div class="col-md-6">
                                 <v-select v-model="selectedCat" :options="dropDownCats" :searchable="false" :on-change="filteredByCategory" class="category-select" placeholder="FILTER LIST" id="selectByCat" transition="menu-fade"></v-select>
                             </div>
-                            <div class="col-md-3"></div>
                             <div class="col-md-6">
                                 <span class="legend"><span class="promo_exist"><i class="fas fa-tag"></i></span> Promotion</span>  
                                 <span class="legend"><span class="new_store"><i class="fas fa-star"></i></span> New Store </span>
