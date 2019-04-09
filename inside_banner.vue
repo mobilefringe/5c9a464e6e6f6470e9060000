023acea6a91f066c883d56d19123cb14708c836f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-09 15:27:59 -0400

    Lewis Template 1.0  /inside_banner.vue

--- inside_banner.vue
+++ inside_banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(255, 119, 0, 0.5), rgba(255, 119, 0, 0.5)), url(' + pageBanner.image_url + ') center center' }">
+    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(255, 119, 0, 0.8), rgba(255, 119, 0, 0.8)), url(' + pageBanner.image_url + ') center center' }">
         <div class="main_container">
             <div class="page_container">
                 <h2 v-html="pageName"></h2>
