fedd79604eef46cc4a7ca9d35b5568a3cc0c67be
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-10 13:49:20 -0400

    Lewis Template 1.0  /main.js

--- main.js
+++ main.js
@@ -110,9 +110,10 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
             ])
         },
         methods: {
+            // altalomasquare
             loadData: async function() {
                 try{
-                    await this.$store.dispatch('initializeApi', { site: "altalomasquare", version: "v4" });
+                    await this.$store.dispatch('initializeApi', { site: "bramaleacitycentre", version: "v4" });
                     await this.$store.dispatch("getData","property");
                     let results = await Promise.all([
                         this.$store.dispatch("INITIALIZE_LOCALE"), 
