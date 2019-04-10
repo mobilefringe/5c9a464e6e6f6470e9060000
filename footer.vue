6f73b687f3ce37a87dd3eec274e3fdf1f6a7f9a7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-10 11:47:18 -0400

    Lewis Template 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -57,6 +57,22 @@
                 }
             },
             created () {
+                window.fbAsyncInit = function() {
+                  FB.init({
+                    appId      : '1111111111',
+                    xfbml      : true,
+                    version    : 'v3.2'
+                  });
+               };
+
+                    (function(d, s, id){
+                     var js, fjs = d.getElementsByTagName(s)[0];
+                     if (d.getElementById(id)) {return;}
+                     js = d.createElement(s); js.id = id;
+                     js.src = "//connect.facebook.net/en_US/sdk.js";
+                     fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                  }
                 this.dataLoaded = true;
             },
             computed: {
