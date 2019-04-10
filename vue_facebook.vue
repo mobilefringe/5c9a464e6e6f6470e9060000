f6be113421fcbc6b8e90f35fbd832bb2752a4766
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-10 12:14:51 -0400

    Lewis Template 1.0  /vue_facebook.vue

--- vue_facebook.vue
+++ vue_facebook.vue
@@ -32,13 +32,28 @@
                 }
               },
               mounted() {
-                (function(d, s, id) {
-                  var js, fjs = d.getElementsByTagName(s)[0];
-                  if (d.getElementById(id)) return;
-                  js = d.createElement(s); js.id = id;
-                  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=1987015024884837&autoLogAppEvents=1';
-                  fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
+                // (function(d, s, id) {
+                //   var js, fjs = d.getElementsByTagName(s)[0];
+                //   if (d.getElementById(id)) return;
+                //   js = d.createElement(s); js.id = id;
+                //   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=1987015024884837&autoLogAppEvents=1';
+                //   fjs.parentNode.insertBefore(js, fjs);
+                // }(document, 'script', 'facebook-jssdk'));
+                window.fbAsyncInit = function() {
+                  FB.init({
+                    appId      : '1987015024884837',
+                    xfbml      : true,
+                    version    : 'v3.2'
+                  });
+              };
+
+                    (function(d, s, id){
+                     var js, fjs = d.getElementsByTagName(s)[0];
+                     if (d.getElementById(id)) {return;}
+                     js = d.createElement(s); js.id = id;
+                     js.src = "//connect.facebook.net/en_US/sdk.js";
+                     fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
               }
         });
     });
