58bc676daf079780a07abea30469aac31607aedb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-10 12:12:49 -0400

    Lewis Template 1.0  /vue_facebook.vue

--- vue_facebook.vue
+++ vue_facebook.vue
@@ -36,7 +36,7 @@
                   var js, fjs = d.getElementsByTagName(s)[0];
                   if (d.getElementById(id)) return;
                   js = d.createElement(s); js.id = id;
-                  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=' + this.appId + '&autoLogAppEvents=1';
+                  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=1987015024884837&autoLogAppEvents=1';
                   fjs.parentNode.insertBefore(js, fjs);
                 }(document, 'script', 'facebook-jssdk'));
               }
