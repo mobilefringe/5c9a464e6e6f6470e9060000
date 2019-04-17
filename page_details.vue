16b92870a97233d2de493a2d8d24efe2f8d898d3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-17 12:05:42 -0400

    Lewis Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -34,7 +34,6 @@
                 }
             },
             mounted() {
-                this.pageName = "";
                 this.updateCurrentPage(this.id);
             },
             watch: {
