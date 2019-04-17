e16d08d9c9bc1e33bf96876efbe8faff15473a83
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-17 11:52:32 -0400

    Lewis Template 1.0  /notfoundcomponent.vue

---
+++ notfoundcomponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row">
+        <div class="large-6 columns">
+            <h1>{{text}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue"], function(Vue) {
+        return Vue.component("not-found-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {"text": "Custom 404 Page"};
+            }
+        });
+    });
+</script>
