bf99dde39d24d51f8087597fe146e7cab475a043
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-17 12:11:28 -0400

    Lewis Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,15 +3,22 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <banner-component :page_name="pageName"></banner-component>
+                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+                    <div class="main_container">
+                        <h2 v-html="currentPage.title"></h2>
+                    </div>
+                </div>
                 <div class="main_container mobile_padding margin_30">
-                    <div class="details_row">
+                    <div v-if="currentImage" class="details_row">
                         <div class="details_col_3 hidden_phone">
                             <image-component :page_name="pageName"></image-component>  
                         </div>
                         <div class="details_col_9">
                             <div class="page_body" v-html="currentPage.body"></div>
                         </div>
+                    </div>
+                    <div v-else class="row">
+                        <div class="page_body" v-html="currentPage.body"></div>
                     </div>
                 </div>
             </div>
@@ -28,12 +35,21 @@
             data: function data() {
                 return {
                     dataLoaded: false,
-                    pageName: "",
+                    pageBanner: null,
                     currentPage: null,
                     currentImage: null
                 }
             },
-            mounted() {
+            created() {
+                var temp_repo = this.findRepoByName('Inside Page Banner').images;
+                if (temp_repo != null) {
+                    this.pageBanner = temp_repo[0];
+                } else {
+                    this.pageBanner = {
+                        "image_url": "//codecloud.cdn.speedyrails.net/sites/5b2925776e6f6432b6110000/image/png/1531495616000/inside_banner.png"
+                    }
+                } 
+                            
                 this.updateCurrentPage(this.id);
             },
             watch: {
@@ -52,7 +68,9 @@
                     this.$nextTick(function() {
                         // Determine the incoming route
                         var route_url = "";
-                        if (_.includes(this.$route.path, "privacy-policy")) {
+                        if (_.includes(this.$route.path, "leasing")) {  
+                            route_url = "/pages/" + this.property.slug + "-leasing.json";
+                        } else if (_.includes(this.$route.path, "privacy-policy")) {
                             route_url = "/pages/" + this.property.slug + "-privacy-policy.json"
                         } else if (_.includes(this.$route.path, "terms-of-use")) {
                             route_url = "/pages/" + this.property.slug + "-terms-of-use.json"
@@ -64,9 +82,9 @@
                         this.property.mm_host = this.property.mm_host.replace("http:", "");
                         this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host +   route_url }).then(function (response) {
                             _this.currentPage = response.data;
-                            console.log(_this.currentPage)
-                            _this.pageName = _this.currentPage.title;
-        
+                            if (!_.includes(_this.currentPage.image_url, "missing")) {
+                                _this.currentImage = _this.currentPage.image_url;
+                            }
                             _this.dataLoaded = true;
                         }, function (error) {
                             console.error( "Could not retrieve data from server. Please check internet connection and try again.");
