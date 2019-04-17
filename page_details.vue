d61b8393fb843149b1bb6c615c3fd17efe98604f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-17 12:13:08 -0400

    Lewis Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(255, 119, 0, 0.8), rgba(255, 119, 0, 0.8)), url(' + pageBanner.image_url + ') center center' }">
                     <div class="main_container">
                         <h2 v-html="currentPage.title"></h2>
                     </div>
