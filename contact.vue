01ddc965ee8632c44356fa05c118c570f795ffe7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 12:55:11 -0400

    Alta Loma Square 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -36,9 +36,7 @@
                                         <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
                                     </div>
                                     <div class="col-xs-12">
-                                        <button class="submit animated_btn" type="submit" :disabled="formSuccess">
-                                            Send <i class="fa fa-angle-right" aria-hidden="true"></i>
-                                        </button>
+                                        <button class="submit animated_btn" type="submit" :disabled="formSuccess">Send</button>
                                     </div>
                                 </form>
                                 <br/>
