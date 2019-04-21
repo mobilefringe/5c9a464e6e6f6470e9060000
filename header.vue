e7cea7ac2bc6a9a0101f93b9fd2463362beed6ee
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 15:55:17 -0400

    Alta Loma Square 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -91,12 +91,10 @@
                                     </div>
                                     <div class="search_component_wrapper">
                                         <div class="search_component_container">
-                                            <search-component v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
+                                            <search-component id="mobile-search" v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
                                                 <template slot="item" scope="option" class="manual">
                                                     <article class="media">
-                                                    <p>
-                                                        {{ option.data.name }}
-                                                    </p>
+                                                        <p>{{ option.data.name }}</p>
                                                     </article>
                                                 </template>
                                             </search-component>
