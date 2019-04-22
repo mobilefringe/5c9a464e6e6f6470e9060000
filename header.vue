a81c7807b91415c5e766a7e62eeb06cf88f4e792
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-22 17:10:09 -0400

    Alta Loma Square 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -43,9 +43,9 @@
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id" @mouseleave="showDropDown = false" @mouseover="showDropDown = true">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{ item.name }}</router-link>
-						        <span @click.native="menuClick()" v-if="item.sub_menu != undefined">{{ item.name }}</span>
+						        <span @click="menuClick()" v-if="item.sub_menu != undefined">{{ item.name }}</span>
 						        <ul v-show="showDropDown" v-if="item.sub_menu">
-						            <li @click.native="menuClick() v-for="sub_menu in item.sub_menu" class="dropdown_item">
+						            <li @click.native="menuClick()" v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{ sub_menu.name }}</router-link>
 						            </li>
 								</ul>
@@ -278,6 +278,7 @@
                 },
                 menuClick: function() {
                     this.$nextTick(function() {
+                        if
                         this.showDropDown = !this.showDropDown;
                     });
                 }
