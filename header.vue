21ff48c8bdd81660bee975cba19ae27224f773e0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-22 16:41:59 -0400

    Alta Loma Square 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -43,9 +43,9 @@
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id" @mouseleave="showDropDown = false" @mouseover="showDropDown = true">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{ item.name }}</router-link>
-						        <span @click="showDropDown = !showDropDown" v-if="item.sub_menu != undefined">{{ item.name }}</span>
+						        <span @click.native="menuClick" v-if="item.sub_menu != undefined">{{ item.name }}</span>
 						        <ul v-show="showDropDown" v-if="item.sub_menu">
-						            <li @click="showDropDown = !showDropDown" v-for="sub_menu in item.sub_menu" class="dropdown_item">
+						            <li @click.native="showDropDown = !showDropDown" v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{ sub_menu.name }}</router-link>
 						            </li>
 								</ul>
@@ -275,6 +275,10 @@
                     this.$nextTick(function() {
                         this.search_result = "";
                     });
+                },
+                menuClick: function(event) {
+                    console.log("event", event)
+                    this.$emit('click', this.page, event); 
                 }
             },
             beforeDestroy: function() {
