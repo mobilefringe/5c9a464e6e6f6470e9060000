832f5aca049f6d0ec69eb83f837e88b1cca793b7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-24 16:56:46 -0400

    Alta Loma Square 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -21,7 +21,7 @@
                             </div>
                             <div v-if="currentStore.unit">
                                 <h3 class="inside_page_title">Address</h3>
-                                <a :href="currentStore.address_link">
+                                <a :href="currentStore.address_link" target="_blank">
                                     <p class="store_details_phone">{{ currentStore.unit }}</p>    
                                 </a>
                             </div>
