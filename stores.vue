a0f5c6c4b30d19fba4bb55873937d7a8f67b8639
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-01 15:20:12 -0400

    Alta Loma Square 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -189,6 +189,6 @@
     });
 </script>
 <!-- Event snippet for Page view conversion page -->
-<script>
-    gtag('event', 'conversion', {'send_to': 'AW-718925610/62gqCO-FhcYBEKre59YC'});
-</script>
+<!--<script>-->
+<!--    gtag('event', 'conversion', {'send_to': 'AW-718925610/62gqCO-FhcYBEKre59YC'});-->
+<!--</script>-->
